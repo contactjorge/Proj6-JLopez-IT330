2741c7e118eaba23cca9b6b0fc862c802f9a3828
Jorge Lopez <contactjorge@thejorgelopez.com>
2018-06-06 15:00:45 -0500

    Writer finalized

--- RoofJobApplication/MainWindow.xaml.cs
+++ RoofJobApplication/MainWindow.xaml.cs
@@ -32,7 +32,7 @@ namespace RoofJobApplication
 		private XmlTextWriter _xmlWriter = new XmlTextWriter("RoofJob.xml", null);
 		
 		// Create StreamReader object.
-		private StreamReader _xmlReader = new StreamReader("RoofJob.txt");
+		//private StreamReader _xmlReader = new StreamReader("RoofJob.txt");
 		
 		// Declare instance variables for inputs.
 		private string _cName, _cAddress, _cCity, _cState, _cZip, _cEstimate, _cWorkDesc;
@@ -55,19 +55,16 @@ namespace RoofJobApplication
 			// Write start element for root.
 			_xmlWriter.WriteStartElement("RoofJobs");
 
-			while (_xmlReader.Peek() != -1)
-			{
-				// Write Person element.
-				_xmlWriter.WriteStartElement("RoofJob");
-				_xmlWriter.WriteElementString("Customer Name", _cName);
-				_xmlWriter.WriteElementString("Customer Address", _cAddress);
-				_xmlWriter.WriteElementString("Customer City", _cCity);
-				_xmlWriter.WriteElementString("Customer State", _cState);
-				_xmlWriter.WriteElementString("Customer Zip", _cZip);
-				_xmlWriter.WriteElementString("Repair Estimate", _cEstimate);
-				_xmlWriter.WriteElementString("Work Description", _cWorkDesc);
-				_xmlWriter.WriteEndElement();
-			}
+			// Write Person element.
+			_xmlWriter.WriteStartElement("RoofJob");
+			_xmlWriter.WriteElementString("Customer Name", _cName);
+			_xmlWriter.WriteElementString("Customer Address", _cAddress);
+			_xmlWriter.WriteElementString("Customer City", _cCity);
+			_xmlWriter.WriteElementString("Customer State", _cState);
+			_xmlWriter.WriteElementString("Customer Zip", _cZip);
+			_xmlWriter.WriteElementString("Repair Estimate", _cEstimate);
+			_xmlWriter.WriteElementString("Work Description", _cWorkDesc);
+			_xmlWriter.WriteEndElement();
 
 			// Write end element for root.
 			_xmlWriter.WriteEndElement();
