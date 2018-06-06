7df62fcd971df0caa66066f11fec2e1c9d8a591a
Jorge Lopez <contactjorge@thejorgelopez.com>
2018-06-06 16:37:41 -0500

    Commit

--- RoofJobApplication/MainWindow.xaml.cs
+++ RoofJobApplication/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml;
+using System.Drawing;
 using Microsoft.Win32;
 
 namespace RoofJobApplication
@@ -29,21 +30,17 @@ namespace RoofJobApplication
 			InitializeComponent();
 			
 	}
-		// Create XmlTextWriter object.
-		private XmlTextWriter _xmlWriter = new XmlTextWriter("RoofJob.xml", null);
+		//Let filepath notate the file to save or open
+		const string _filePath = "RoofJobs.xml";
 
+		// Create XmlTextWriter object.
+		private XmlTextWriter _xmlWriter = new XmlTextWriter(_filePath, null);
+		
 		// Declare instance variables for inputs.
 		private string _cName, _cAddress, _cCity, _cState, _cZip, _cEstimate, _cWorkDesc;
 
 		private void xmlWriter()
 		{
-			//Set the format for XML File
-			_xmlWriter.Formatting = Formatting.Indented;
-			_xmlWriter.IndentChar = ' ';
-
-			// Write start element for root.
-			_xmlWriter.WriteStartElement("RoofJobs.xml");
-
 			// Write Person element.
 			_xmlWriter.WriteStartElement("RoofJob");
 			_xmlWriter.WriteElementString("Customer Name", _cName);
@@ -57,11 +54,8 @@ namespace RoofJobApplication
 
 			// Write end element for root.
 			_xmlWriter.WriteEndElement();
-
-			// Close writer.
-			_xmlWriter.Close();
 		}
-
+		
 		private void btnSubmit_Click(object sender, RoutedEventArgs e)
 		{
 			//Gather Data From Form
@@ -72,11 +66,18 @@ namespace RoofJobApplication
 			_cZip = txtCustZip.Text;
 			_cEstimate = txtRepairEstimate.Text;
 			_cWorkDesc = txtWorkDescription.Text;
+			//Set the format for XML File
+			_xmlWriter.Formatting = Formatting.Indented;
+			_xmlWriter.IndentChar = ' ';
+
+			// Write start element for root.
+			_xmlWriter.WriteStartElement(_filePath);
 
 			//xmlWriter(_cName, _cAddress, _cCity, _cState, _cZip, _cEstimate, _cWorkDesc);
 			xmlWriter();
 
-
+			// Close writer.
+			_xmlWriter.Close();
 		}
 	}
 }
--- RoofJobViewer/MainWindow.xaml.cs
+++ RoofJobViewer/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace RoofJobViewer
 {
@@ -23,6 +25,26 @@ namespace RoofJobViewer
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		//Let filepath notate the file to save or open
+		const string _filePath = "RoofJobs.xml";
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			//Open a stream to APPEND to the file
+			StreamWriter writer = new StreamWriter(_filePath, true);
+
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+
+			openFileDialog.Filter = "XML files (*.xml)|*.xml|All files (*.*)|*.*";
+			openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+			if (openFileDialog.ShowDialog() == true)
+			{
+				txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+			}
+				
 		}
 	}
 }
